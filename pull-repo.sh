9816bd2f3ef5a001666bcc538763380e12e5e424
Nianhua Wei <veinian@gmail.com>
2012-04-15 23:30:53 +0800

    递归实现

--- pull-repo.sh
+++ pull-repo.sh
@@ -2,45 +2,36 @@
 # 只要一个目录下有 .git 则认为它是 git repo, 需要 pull,
 # 否则就继续寻找它的子目录---采用递归的方式
 
-BASEDIR='.'
-PATH='/usr/bin/:/usr/sbin:/sbin:/bin'
-DIRECTORY=`ls`
+WORKINGDIR=$1
+if [ "x$WORKINGDIR" == "x" ]; then
+	WORKINGDIR='.'
+fi
+cd $WORKINGDIR
 
-function findGitRepo()
-{
-    ISAGITREPO=`ls -a | grep '^.git$' | wc -l`
-    [[ $ISAGITREPO -gt 0 ]] && return 0 || return 1
-}
+PATH='/usr/bin/:/usr/sbin:/sbin:/bin'
 
 function gitPullCmd()
 {
-    DIRECTORY=$1
-        cd $DIRECTORY
-        if ( findGitRepo ); then
-            echo "$DIRECTORY 开始拉取数据"
-            git pull
-        else
-            SUBDIR=`ls`
-                for i in $SUBDIR;do
-                    if [ -d $i ]; then
-                        cd $i;
-                        if ( findGitRepo ); then
-                             echo "$i 开始拉取数据"
-                             git pull
-                        else
-                             echo "$i 递归的处理这个目录"
-                             # how to handle this
-                             # "gitPullCmd $i" i don't think so
-                        fi
-                        cd ..
-                    fi
-               done
-        fi
-        cd ..
+	DIRECTORY=$1
+	cd $DIRECTORY
+	ISAGITREPO=`ls -a | grep '^.git$' | wc -l`
+	if [ $ISAGITREPO -gt 0 ]; then
+        echo "$DIRECTORY 开始拉取数据"
+        git pull
+	else
+		echo "处理 $DIRECTORY 的子目录"
+		SUBDIR=`ls`	
+		for i in $SUBDIR;do
+			if [ -d $i ]; then
+				gitPullCmd $i
+			fi
+		done
+	fi
+	cd ..
 }
 
-for FILE in $DIRECTORY; do
+for FILE in `ls`; do
     if [ -d $FILE ]; then
-        gitPullCmd $FILE 
+        gitPullCmd $FILE
     fi
 done
