c5f52994b47f0ff61d9bec8a6253d9b59e67bac2
Nianhua Wei <veinian@gmail.com>
2012-04-15 23:42:58 +0800

    small change

--- pull-repo.sh
+++ pull-repo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # 只要一个目录下有 .git 则认为它是 git repo, 需要 pull,
 # 否则就继续寻找它的子目录---采用递归的方式
+# usage:
+#  pull-git-repo.sh [ git-repo-directory ]
 
 WORKINGDIR=$1
 if [ "x$WORKINGDIR" == "x" ]; then
-	WORKINGDIR='.'
+    WORKINGDIR='.'
 fi
 cd $WORKINGDIR
 
@@ -12,23 +14,24 @@ PATH='/usr/bin/:/usr/sbin:/sbin:/bin'
 
 function gitPullCmd()
 {
-	DIRECTORY=$1
-	cd $DIRECTORY
-	ISAGITREPO=`ls -a | grep '^.git$' | wc -l`
-	if [ $ISAGITREPO -gt 0 ]; then
-        echo "$DIRECTORY 开始拉取数据"
-        git pull
-	else
-		echo "处理 $DIRECTORY 的子目录"
-		SUBDIR=`ls`	
-		for i in $SUBDIR;do
-			if [ -d $i ]; then
-				gitPullCmd $i
-			fi
-		done
-	fi
-	cd ..
-}
+    DIRECTORY=$1
+    echo $DIRECTORY
+    cd $DIRECTORY
+    ISAGITREPO=`ls -a | grep '^.git$' | wc -l`
+    if [ $ISAGITREPO -gt 0 ]; then
+            echo "$DIRECTORY 开始拉取数据"
+            git pull
+    else
+        echo "处理 $DIRECTORY 的子目录"
+        SUBDIR=`ls`
+        for i in $SUBDIR;do
+            if [ -d $i ]; then
+                gitPullCmd $i
+            fi
+        done
+    fi
+    cd ..
+    }
 
 for FILE in `ls`; do
     if [ -d $FILE ]; then
